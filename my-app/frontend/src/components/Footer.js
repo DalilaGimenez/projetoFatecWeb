23743bf4ac289ae511338f227930658e584f265c
dalilagimenez <dalila.gimenez@gmail.com>
2024-05-14 23:08:02 -0300

    "Adicionando arquivos iniciais e configurando o backend"

---
+++ my-app/frontend/src/components/Footer.js
@@ -0,0 +1,14 @@
+import "./Footer.css";
+
+const Footer = () => {
+    return (
+        <footer id="footer">
+            <div>
+            <p>Cãonecta AUmor &copy; 2024</p>
+            <p>Feito com amor por: Dalila Gimenez</p>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
